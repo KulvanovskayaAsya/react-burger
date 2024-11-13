05402b38b00336bc8a9cbca41a81d80946365e8a
KulvanovskayaAsya <nastena.kulvanovskaya.02@mail.ru>
2024-11-13 21:58:54 +0300

    remove unused slice

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,13 +1,11 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import ingredientsReducer from './burgerIngredientsSlice';
-import ingredientDetailsReducer from './ingredientDetailsSlice';
 import burgerConstructorReducer from './burgerConstructorSlice';
 import orderReducer from './orderSlice';
 import authReducer from './authSlice';
 
 export const rootReducer = combineReducers({
   burgerIngredients: ingredientsReducer,
-  ingredientDetails: ingredientDetailsReducer,
   burgerConstructor: burgerConstructorReducer,
   order: orderReducer,
   auth: authReducer
